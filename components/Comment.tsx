3ce6cb36e23cb6b6f96ff1609f591f3eefe5c337
Albert Ngo <albertngo1@gmail.com>
2020-06-15 02:36:24 -0400

    finish nested comments

--- components/Comment.tsx
+++ components/Comment.tsx
@@ -1,15 +1,16 @@
 /* eslint-disable react/prop-types */
 
-import React, { ReactElement, FC, useEffect, useState } from 'react';
+import React, { Fragment, ReactElement, FC, useEffect, useState } from 'react';
 import { format } from 'timeago.js';
 import { getItem } from '../utils/network';
+import { stringToColour } from '../utils/utils';
 
 interface Props {
   commentId: number
   level?: number
 }
 
-const Comment: FC<Props> = ({ commentId, level = 0 }) => {
+const Comment: FC<Props> = ({ commentId, level }) => {
   const [comment, setComment] = useState(null);
 
   useEffect(() => {
@@ -32,21 +33,40 @@ const Comment: FC<Props> = ({ commentId, level = 0 }) => {
 
     return (
       kids.map((kid: number, idx: number): ReactElement => {
-        return <Comment key={`child-comment-${id}-${idx}`} commentId={kid} level={level + 1} />;
+        return (
+          <Fragment key={`child-comment-${id}-${idx}`}>
+            <div>
+              <Comment
+                commentId={kid}
+                level={level + 1}
+              />
+            </div>
+
+            <style jsx>{`
+              div {
+                margin-left: 2rem;
+                border-top: 3px solid ${stringToColour(level.toString())};
+                border-left: 3px solid ${stringToColour(level.toString())};
+                border-top-left-radius: 10px;
+              }
+            `}</style>
+          </Fragment>
+        );
       })
     )
   }
 
-  const markup = { __html: text };
-
   return (
     <>
       {by} {formattedTime}
-      <div dangerouslySetInnerHTML={markup}></div>
+      <div dangerouslySetInnerHTML={{ __html: text }}></div>
 
       {renderNestedComments()}
-      <style jsx>{`
 
+      <style jsx>{`
+        div {
+          margin: 1rem 0;
+        }
       `}</style>
     </>
   )
--- components/StoryContent.tsx
+++ components/StoryContent.tsx
@@ -2,20 +2,30 @@
 
 import React, { ReactElement, FC } from 'react';
 import Comment from './Comment';
+import { stringToColour } from '../utils/utils';
 
 interface Props {
   commentIds: number[]
 }
 
 const StoryContent: FC<Props> = ({ commentIds }) => {
+  const level = 0;
   return (
     <>
       {commentIds.map((commentId: number, idx: number): ReactElement => {
-        return <Comment key={`parent-comment-${idx}`} commentId={commentId} />;
+        return (
+          <div key={`parent-comment-${commentId}-${idx}`}>
+            <Comment commentId={commentId} level={level + 1} />
+          </div>
+        )
       })}
 
       <style jsx>{`
-
+        div {
+          border-top: 3px solid ${stringToColour(level.toString())};
+          border-left: 3px solid ${stringToColour(level.toString())};
+          border-top-left-radius: 10px;
+        }
       `}</style>
     </>
   )
